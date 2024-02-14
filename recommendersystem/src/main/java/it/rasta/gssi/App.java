603fe6732ba7fd32e9aefe68c4e2c61829f51923
Ludovico Iovino <iovinoludovico@users.noreply.github.com>
2024-02-14 06:32:50 +0100

    initial

---
+++ recommendersystem/src/main/java/it/rasta/gssi/App.java
@@ -0,0 +1,13 @@
+package it.rasta.gssi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
